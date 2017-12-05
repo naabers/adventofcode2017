d484b51a075dff23c2644caf61521b49bc014cf1
Ray Naab <ray.naab@osnexus.com>
2017-12-05 11:43:01 -0800

    Changed my test to actually validate answered tests.
    was using the test framework as a cheap runner to be able to sepecify which day to test

--- adventofcode_test.go
+++ adventofcode_test.go
@@ -3,54 +3,66 @@ package adventofcode2017
 import (
 	"fmt"
 	"testing"
+
+	"github.com/stretchr/testify/assert"
 )
 
 func TestDay1PartA(t *testing.T) {
 	result := day1PartA()
 	fmt.Printf("Day 1A: '%s'\n", result)
+	assert.Equal(t, "1031", result, "Day 1 Part A is broken")
 }
 
 func TestDay1PartB(t *testing.T) {
 	result := day1PartB()
 	fmt.Printf("Day 1B: '%s'\n", result)
+	assert.Equal(t, "1080", result, "Day 1 Part B is broken")
 }
 
 func TestDay2PartA(t *testing.T) {
 	result := day2PartA()
 	fmt.Printf("Day 2A: '%s'\n", result)
+	assert.Equal(t, "54426", result, "Day 2 Part A is broken")
 }
 
 func TestDay2PartB(t *testing.T) {
 	result := day2PartB()
 	fmt.Printf("Day 2B: '%s'\n", result)
+	assert.Equal(t, "333", result, "Day 2 Part B is broken")
 }
 
 func TestDay3PartA(t *testing.T) {
 	result := day3PartA()
 	fmt.Printf("Day 3A: '%s'\n", result)
+	assert.Equal(t, "438", result, "Day 3 Part A is broken")
 }
 
 func TestDay3PartB(t *testing.T) {
 	result := day3PartB()
 	fmt.Printf("Day 3B: '%s'\n", result)
+	assert.Equal(t, "266330", result, "Day 3 Part B is broken")
 }
 
 func TestDay4PartA(t *testing.T) {
 	result := day4PartA()
 	fmt.Printf("Day 4A: '%s'\n", result)
+	assert.Equal(t, "325", result, "Day 4 Part A is broken")
 }
 
 func TestDay4PartB(t *testing.T) {
 	result := day4PartB()
 	fmt.Printf("Day 4B: '%s'\n", result)
+	assert.Equal(t, "119", result, "Day 4 Part B is broken")
 }
 
 func TestDay5PartA(t *testing.T) {
 	result := day5PartA()
 	fmt.Printf("Day 5A: '%s'\n", result)
+	assert.Equal(t, "388611", result, "Day 5 Part A is broken")
 }
 
 func TestDay5PartB(t *testing.T) {
 	result := day5PartB()
 	fmt.Printf("Day 5B: '%s'\n", result)
+	assert.Equal(t, "27763113", result, "Day 5 Part B is broken")
 }
